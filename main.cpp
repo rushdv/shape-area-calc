e763f271ee963a17b0570e2b4262b536e3fd6abd
Md Shihab Shahriar Rashu <md.shihabshahriarrashu@gmail.com>
2025-12-28 15:49:39 +0600

    added while loop

--- main.cpp
+++ main.cpp
@@ -1,74 +1,98 @@
 #include <iostream>
 using namespace std;
 
-class Shape{
+class Shape
+{
 public:
-    virtual void area() = 0;    
+    virtual void area() = 0;
     virtual ~Shape() {}
 };
 
-class Rectangle : public Shape{
+class Rectangle : public Shape
+{
 private:
     float length, width;
 
 public:
-    Rectangle(float l, float w) {
+    Rectangle(float l, float w)
+    {
         length = l;
         width = w;
     }
 
-    void area() override {
+    void area() override
+    {
         cout << "Area of Rectangle: " << length * width << endl;
     }
 };
 
-class Circle : public Shape{
+class Circle : public Shape
+{
 private:
     float radius;
 
 public:
-    Circle(float r) {
+    Circle(float r)
+    {
         radius = r;
     }
 
-    void area() override {
+    void area() override
+    {
         cout << "Area of Circle: " << 3.1416 * radius * radius << endl;
     }
 };
 
-int main() {
-    Shape* shape = nullptr;
+int main()
+{
+
     int choice;
 
-    cout << "Choose Shape : \n";
-    cout << "1. Rectangle \n";
-    cout << "2. Circle \n";
-    cout << "Enter choice: ";
-    cin >> choice ;
+    while (true)
+    {
+        cout << "====== SHAPE AREA CALCULATOR =====\n";
+        cout << "1. Rectangle \n";
+        cout << "2. Circle \n";
+        cout << "3. Exit \n";
+        cout << "Enter your choice: ";
+        cin >> choice;
 
-    if (choice == 1){
-        float l, w;
-        cout << "Enter length: ";
-        cin >> l;
-        cout << "Enter width: ";
-        cin >> w;
+        Shape *shape = nullptr;
 
-        shape = new Rectangle(l, w);
+        if (choice == 1)
+        {
+            float l, w;
+            cout << "Enter length: ";
+            cin >> l;
+            cout << "Enter width: ";
+            cin >> w;
+
+            shape = new Rectangle(l, w);
+        }
+        else if (choice == 2)
+        {
+            float r;
+            cout << "Enter radius: ";
+            cin >> r;
+
+            shape = new Circle(r);
+        }
+        else if (choice == 3)
+        {
+            cout << "Exiting program....\n";
+            break;
+        }
+        else
+        {
+            cout << "Invalid choice ! Try again....\n"
+                 << endl;
+            continue;
+        }
+
+        shape->area();
+
+        delete shape;
     }
-    else if (choice == 2) {
-        float r;
-        cout << "Enter radius: ";
-        cin >> r;
 
-        shape = new Circle(r);
-    }
-    else {
-        cout << "Invalid choice !" << endl;
-        return 0;
-    }
-
-    shape -> area();
-
-    delete shape;
     return 0;
 }
